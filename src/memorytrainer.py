533279c9618b80656777f55c921751dc888c3bdc
arache <arache.register@gmail.com>
2019-11-18 23:04:44 +0800

    Add MemoryTrainer

---
+++ src/memorytrainer.py
@@ -0,0 +1,68 @@
+import json
+from enum import Enum
+from datetime import datetime
+from typing import Set, Dict, List, Any
+
+
+class Constant:
+    LEVEL = 'level'
+    DATE = 'date'
+    STATUS = 'status'
+
+
+class StatusEnum(str, Enum):
+    NOT_STARTED = 'not started'
+    PASS = 'pass'
+    FAIL = 'fail'
+
+
+class MemoryTrainer:
+
+    def __init__(self) -> None:
+        # {question, ['level', 'date', 'status']}
+        self._training_log: Dict[str, List[Dict[str, Any]]] = {}
+
+    @property
+    def training_log(self):
+        return self._training_log
+
+    @classmethod
+    def from_training_set(cls, training_set: Set[str]) -> 'MemoryTrainer':
+        trainer = cls()
+        for question in training_set:
+            trainer._training_log[question] = []
+        return trainer
+
+    @classmethod
+    def from_json_dump_file(cls, json_dump_file: str) -> 'MemoryTrainer':
+        trainer = cls()
+        with open(json_dump_file, 'r') as f:
+            trainer._training_log = json.load(f)
+        return trainer
+
+    def persist_record(self, file: str):
+        with open(file, 'w') as f:
+            json.dump(self.training_log, f, indent=4)
+
+    def upsert_record(self, question: str, level: int, date: datetime, status: StatusEnum):
+        # no record before
+        if not self.training_log[question]:
+            self.training_log[question].append(MemoryTrainer.build_record(level, date, status))
+            return
+
+        # already some records there
+        data_new = date.strftime('%Y%m%d')
+        last_one = self.training_log[question][-1]
+        if last_one[Constant.DATE] > data_new:
+            raise ValueError(
+                f"Time never goes back, can not change {data_new} while the last one is {last_one[Constant.DATE]}")
+
+        if last_one[Constant.DATE] == data_new:
+            last_one[Constant.LEVEL] = level
+            last_one[Constant.STATUS] = status
+        else:
+            self.training_log[question].append(MemoryTrainer.build_record(level, date, status))
+
+    @staticmethod
+    def build_record(level: int, date: datetime, status: StatusEnum):
+        return {Constant.LEVEL: level, Constant.DATE: date.strftime('%Y%m%d'), Constant.STATUS: status}
---
+++ src/tests/test_memoryTrainer.py
@@ -0,0 +1,41 @@
+from datetime import datetime
+from unittest import TestCase
+
+from memorytrainer import MemoryTrainer, StatusEnum
+
+
+class TestMemoryTrainer(TestCase):
+
+    def setUp(self) -> None:
+        super().setUp()
+        self.training_set = {'1 + 1', '2 + 2', '3 + 3'}
+        self.trainer = MemoryTrainer.from_training_set(self.training_set)
+
+    def test_build_record(self):
+        # given
+
+        # when
+        record = MemoryTrainer.build_record(3, datetime(2019, 1, 1), StatusEnum.FAIL)
+
+        # then
+        self.assertEqual(record, {'level': 3, 'date': '20190101', 'status': StatusEnum.FAIL})
+
+    def test_from_training_set(self):
+        # given
+        # when
+        # then
+        for k in self.training_set:
+            self.assertTrue(k in self.trainer.training_log.keys())
+
+    def test_persist_load(self):
+        # given
+        self.trainer.upsert_record('1 + 1', 1, datetime(2019, 1, 1), StatusEnum.PASS)
+        self.trainer.upsert_record('1 + 1', 1, datetime(2019, 1, 4), StatusEnum.NOT_STARTED)
+        self.trainer.upsert_record('2 + 2', 1, datetime(2019, 2, 1), StatusEnum.FAIL)
+
+        # when
+        self.trainer.persist_record('atest.json')
+        trainer2 = MemoryTrainer.from_json_dump_file('atest.json')
+
+        # then
+        self.assertDictEqual(self.trainer.training_log, trainer2.training_log)
